40a5daecf753f6e402eeb4994006eea2f761e717
ahadsheriff <ahadsheriff@gmail.com>
2017-02-19 18:41:14 -0500

    Added username text field and configured keyboard

--- TapMe/LoginViewController.swift
+++ TapMe/LoginViewController.swift
@@ -9,25 +9,43 @@
 import UIKit
 import Firebase
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var gifView: UIImageView!
+    
+    @IBOutlet weak var textBox: UITextField!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        /*
         // Returns an animated UIImage
         let fireGif = UIImage.gifWithName(name: "fire")
         // Use the UIImage in your UIImageView
         _ = UIImageView(image: fireGif)
         self.gifView?.image = fireGif
+        */
         
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(LoginViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
         
+        self.textBox.delegate = self
         
     }
-
+    
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool // called when 'return' key pressed. return NO to ignore.
+    {
+        textField.resignFirstResponder()
+        return true;
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
